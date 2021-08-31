3a7c83d1425968a56b2e2e0bc7e29807f4a3147e
Christopher Agomuo <agomuosoki@gmail.com>
2021-08-31 07:52:57 +0100

    initial commit

---
+++ multiplication/multiplication.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace cSharpAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int result = 0;
+
+            Console.Write("Please Enter the first number: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Pleae Enter the second number: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            result = num1 * num2;
+            Console.WriteLine("The result of multiplying the 2 numbers is: " + result);
+        }
+    }
+}
---
+++ subtraction/subraction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 0;
+            int secondNumber = 0;
+            int answer = 0;
+
+            Console.Write("Please Enter the 1st number: ");
+            firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Please Enter the 2nd number: ");
+            secondNumber = int.Parse(Console.ReadLine());
+
+            answer = firstNumber - secondNumber;
+            Console.WriteLine("Subtracting the 2 number gives: " + answer);
+        }
+    }
+}
